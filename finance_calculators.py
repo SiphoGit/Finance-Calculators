93b4273011b7a604168837acf1caa36799372c5c
Sipho <shimanges@yahoo.com>
2023-03-12 13:17:08 +0200

    Modified finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,124 +1,111 @@
 import math
 
+def simple_interest_calculator(amount: float, interests: float, period: float) -> float:
+    # calculates future value
+    future_value = round((amount* (1 + (interests/100)* period)), 2)    
 
-# Displayed message when user opens the calculator.
-print("Choose either 'investment' or 'bond' from the menu below to proceed: \n")
-print('''
-    investment - to calculate the amount of interest you'll earn on investment
-    bond       - to calculate the amount you'll have to pay on a home loan \n''')  
- 
-# User's calculator selection.
-calculator = input("Select calculator: ").lower()
-investment_calculator = "investment"
-bond_calculator = "bond"
+    return(f'''
+    Invested amount   : R{amount}
+    Interests         : {interests}%
+    Interest type     : {"Compound"}
+    Investment period : {period} years
+    Future Value      : R{future_value}''')     
 
-# Error message when invalid calculator option entered.
-while (
-    calculator != investment_calculator and
-    calculator != bond_calculator):
-    print("\nInvalid input, please select investment or bond calculator.\n")
-    calculator = input("Select calculator: ").lower()
-
-else:
-    print("You've selected {} calculator.\n".format(calculator))  
-
-
-# Investment calculator.
-# User's input.
-if calculator == investment_calculator:
-    investment_amount = float(input("Investment amount: R"))
-    interest_rate = float(input("Annual interest: "))
-    investment_period = float(input("Investment period: "))
-    interest = input("Simple or Compound interest?: ").lower()
-    simple_interest = "simple"
-    compound_interest = "compound"
-
-    # Error message when invalid interest type entered.
-    while (
-        interest != simple_interest and
-        interest != compound_interest):
-        print("\nInvalid input, please select simple or coumpound interest.\n")
-        interest = input("Simple or Compound interest?: ").lower()
-
-    else:
-        print("\nYou've selected {} interest.\n".format(interest))
+def compound_interest_calculator(amount: float, interests: float, period: float) -> float:    
+    # calculates future value
+    future_value = round((amount* math.pow((1+(interests/100)),period)), 2) 
         
+    return(f'''
+        Invested amount   : R{amount}
+        Interests         : {interests}%
+        Interest type     : {"Compound"}
+        Investment period : {period} years
+        Future Value      : R{future_value}''')            
 
-    # Simple interest calculator.    
-        if interest == simple_interest:
 
-            future_value = round((
-                investment_amount
-                * (1 + (interest_rate/100)
-                * investment_period)), 2)           # Simple interest formula.    
+def bond_calculator(bond_annual_interests: float, payment_period_in_years: float, 
+                    house_present_value: float) -> float:
+  
+    try:
+        monthly_interests = (bond_annual_interests / 12) / 100
+        number_of_payments = payment_period_in_years * 12
+        
+        #Calculates monthly payments.
+        bond_monthly_payments = round(
+            ((house_present_value *
+            (monthly_interests * math.pow((1 + monthly_interests),
+            number_of_payments)))/
+            (math.pow((1 + monthly_interests), number_of_payments) - 1)))
+               
+        return(
+        f'''
+        House value        : R{house_present_value}
+        Annual interests   : {bond_annual_interests}%
+        Number of payment  : {number_of_payments}
+        Monthly payment    : R{bond_monthly_payments}''')
+    except ZeroDivisionError:
+        print("Bond interests and payment period can't be zero")
+    
+def main():
+
+    while True:
+        #Main menu 
+        calculator_type = input(
+            '''
+Select Calculator:
+I - Investment Calculator
+B - Bond calculator
+E - to exit the program
+>>> '''
+        ).capitalize()
+        
+        if calculator_type == "I":
             
-            print(
-                '''
-                Invested amount   : R{}
-                Annual interests  : {}%
-                Interest type     : {}
-                Investment period : {} years
-                Future Value      : R{}'''.format(investment_amount,
-                                                    interest_rate,
-                                                    interest,
-                                                    investment_period,
-                                                    future_value))
+            while True: 
+                try:
+                    choice = int(input(
+                        '''Select interest type:
+                            1 - Simple interest
+                            2 - Compound interest
+                            3 - Back to main menu
+                            >>> '''))                        
+                                                   
+                    if choice == 1:
+                        amount = float(input("Investment amount: R"))
+                        interests = float(input("Annual interest: "))
+                        period = float(input("Investment period(years): "))
+                        future_value = simple_interest_calculator(amount, interests, period)
+                        print(future_value)
+                    if choice == 2:
+                        amount = float(input("Investment amount: R"))
+                        interests = float(input("Annual interest: "))
+                        period = float(input("Investment period(years): "))
+                        future_value = compound_interest_calculator(amount, interests, period)
+                        print(future_value)
+                    if choice == 3:
+                        break
+                except ValueError:
+                    print("Invalid input value")
+        elif calculator_type == "B":
+            while True:
+                choice = input("\nPress Enter to continue or B to go back to the main menu: ").capitalize()
 
-    
-        # Compound interest calculator.
-        
-        else:  
-
-            future_value = round((
-                investment_amount
-                * math.pow((1+(interest_rate/100)),
-                            investment_period)), 2)   # Compound interest formula.
-                
-            print(
-                '''
-                Invested amount   : R{}
-                Annual interests  : {}%
-                Interest type     : {}
-                Investment period : {} years
-                Future Value      : R{}'''.format(investment_amount,
-                                                    interest_rate,
-                                                    interest,
-                                                    investment_period,
-                                                    future_value))                                           
-else:
-
-    # Bond Calculator.
-    # User's input.
-    
-    house_present_value = float(input("Enter house value: R"))
-    bond_annual_interests = float(input("Loan interests: "))
-    payment_period_in_years = float(input("Payment period (years): "))
-    
-    # Converting annual inputs to monthly.
-    
-    monthly_interests = (bond_annual_interests / 12) / 100
-    number_of_payments = payment_period_in_years * 12
-    
-
-    #Bond monthly payment formula.
-
-    bond_monthly_payments = round(((
-        house_present_value * (monthly_interests * math.pow((1 + monthly_interests)
-                            , number_of_payments)))
-        / (math.pow((1 + monthly_interests), number_of_payments) - 1)))
-    
-
-    print(
-        '''
-           Bond Amount        : R{}
-           Annual interests   : {}%
-           Number of payment  : {}
-           Monthly payment    : R{}'''.format(house_present_value,
-                                              bond_annual_interests,
-                                              number_of_payments,
-                                              bond_monthly_payments))
-        
-            
+                if choice == "":
+                    try:
+                        house_present_value = float(input("\nEnter house value: R"))
+                        bond_annual_interests = float(input("Bond interests: "))
+                        payment_period = float(input("Payment period (years): "))
+                        monthly_payments = bond_calculator(bond_annual_interests, payment_period, house_present_value)
+                        print(monthly_payments)
+                    except ValueError:
+                        print("Invalid input value")
+                if choice == "B":
+                    break
+        elif calculator_type == "E":
+            break
+        else:
+            print("Please select I, B, or E")
+main()          
                               
 
     
